a5b5a24b27ebe2a6210fb1b4973bb432d5e011be
Nebularsty <nicolasoliveira9635@gmail.com>
2022-12-06 18:04:31 -0300

    filtragem pc

--- src/app/modules/home/pages/montepc/montepc.component.ts
+++ src/app/modules/home/pages/montepc/montepc.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import * as _ from 'lodash';
 import Pc from 'src/app/core/models/Pecas.model';
+import { ApiPecasService } from 'src/app/core/services/api-pecas.service';
 
 @Component({
   selector: 'app-montepc',
   templateUrl: './montepc.component.html',
-  styleUrls: ['./montepc.component.scss']
+  styleUrls: ['./montepc.component.scss'],
 })
 export class MontepcComponent implements OnInit {
-  
-  respostaPontos='0'
-  computadores: Pc[] =[]
+  respostaPontos = '0';
+  computadores: Pc[] = [];
   formulario: FormGroup;
   valorCheckado: any = 0;
   precoDoPc: any = 0;
- 
+  valorFinal: number = 0;
 
-  constructor() { 
-    this.formulario = new FormGroup ({
+  constructor(private apiPecas: ApiPecasService) {
+    this.formulario = new FormGroup({
       primeiraCheckBox: new FormControl(false),
       segundaCheckBox: new FormControl(false),
       terceiraCheckBox: new FormControl(false),
@@ -32,66 +33,71 @@ export class MontepcComponent implements OnInit {
       decimaSegundaCheckBox: new FormControl(false),
       decimaTerceiraCheckBox: new FormControl(false),
       decimaQuartaCheckBox: new FormControl(false),
-      decimaQuintaCheckBox: new FormControl(false)
-      
-      
-    })
-
-
+      decimaQuintaCheckBox: new FormControl(false),
+    });
   }
-
 
   ngOnInit() {
-    this.respostaPontos= '0';
-    
-    
+    this.respostaPontos = '0';
+    this.listaDeComputadores();
   }
-    
-  
 
-  changeSelect(){
-    this.formulario.value.primeiraCheckBox
-    this.formulario.value.segundaCheckBox
-    this.formulario.value.terceiraCheckBox
-    this.formulario.value.quartaCheckBox
-    this.formulario.value.quintaCheckBox
-    this.formulario.value.sextaCheckBox 
-    this.formulario.value.setimaCheckBox
-    this.formulario.value.oitavaCheckBox
-    this.formulario.value.nonaCheckBox
-    this.formulario.value.decimaCheckBox
-    this.formulario.value.decimaPrimeiraCheckBox
-    this.formulario.value.decimaSegundaCheckBox
-    this.formulario.value.decimaTerceiraCheckBox
-    this.formulario.value.decimaQuartaCheckBox
-    this.formulario.value.decimaQuintaCheckBox
-    
-}
+  changeSelect() {
+    this.formulario.value.primeiraCheckBox;
+    this.formulario.value.segundaCheckBox;
+    this.formulario.value.terceiraCheckBox;
+    this.formulario.value.quartaCheckBox;
+    this.formulario.value.quintaCheckBox;
+    this.formulario.value.sextaCheckBox;
+    this.formulario.value.setimaCheckBox;
+    this.formulario.value.oitavaCheckBox;
+    this.formulario.value.nonaCheckBox;
+    this.formulario.value.decimaCheckBox;
+    this.formulario.value.decimaPrimeiraCheckBox;
+    this.formulario.value.decimaSegundaCheckBox;
+    this.formulario.value.decimaTerceiraCheckBox;
+    this.formulario.value.decimaQuartaCheckBox;
+    this.formulario.value.decimaQuintaCheckBox;
+  }
 
-finalizarQuestionario(){
-  <HTMLInputElement[]><unknown>document.querySelectorAll('.option').forEach((item: any) => {
-    if (item.checked) { 
-      if(parseInt(item.value) > this.valorCheckado){
-        this.valorCheckado = parseInt(item.value)
-        console.log(item.value)
-      }
+  finalizarQuestionario() {
+    <HTMLInputElement[]>(
+      (<unknown>document.querySelectorAll('.option').forEach((item: any) => {
+        if (item.checked) {
+          if (parseInt(item.value) > this.valorCheckado) {
+            this.valorCheckado = parseInt(item.value);
+            console.log(item.value);
+            this.resultadoFinalDePontos(item.value);
+          }
+        }
+      }))
+    );
+    this.finalizarPreco();
+  }
 
-    }
-  })
-  this.finalizarPreco();
+  resultadoFinalDePontos(valor: number) {
+    this.computadores = _.filter(this.computadores, (pc: any) => {
+      console.log(this.valorFinal);
+      return pc.ponto == valor;
+    });
+  }
 
-}
+  listaDeComputadores() {
+    this.apiPecas.obterTodos().subscribe({
+      next: (res: any) => console.log(res),
+      error: (err: any) => console.log(err),
+    });
+  }
 
-
-finalizarPreco(){
-  <HTMLInputElement[]><unknown>document.querySelectorAll('.preco').forEach((item: any) => {
-    if (item.checked) { 
-        this.precoDoPc = parseInt(item.value)
-        console.log(item.value)
-        console.log(this.precoDoPc)
-      
-
-    }
-  })
-}
+  finalizarPreco() {
+    <HTMLInputElement[]>(
+      (<unknown>document.querySelectorAll('.preco').forEach((item: any) => {
+        if (item.checked) {
+          this.precoDoPc = parseInt(item.value);
+          console.log(item.value);
+          console.log(this.precoDoPc);
+        }
+      }))
+    );
+  }
 }
